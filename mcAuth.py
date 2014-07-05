a78d096ad4468f0801b9b87b52e2697598128c7f
philip <philip.groet+github@gmail.com>
2014-07-05 08:40:05 +0200

    Improved

--- mcAuth.py
+++ mcAuth.py
@@ -7,7 +7,7 @@ base_url = "https://authserver.mojang.com"
 login_file = "testrun.json"     # ~/.minecraft/launcher_profiles.json
 cred_file = "cred_file.txt"
 clienttoken = "7660950e-7e03-4188-b6c1-8de5b640ced5"
-
+authtries = 0
 
 def load_cred(cred_file):
     if not os.path.exists(cred_file):
@@ -37,7 +37,7 @@ def save_file(file_name, file_c, credentials):   # save login file
         "profiles": {
             file_c["selectedProfile"]["name"]: {
                 "name": file_c["selectedProfile"]["name"],
-                "playerUUID": file_c["clientToken"]
+                "playerUUID": file_c["selectedProfile"]["id"]
             }
         },
         "selectedProfile": file_c["selectedProfile"]["name"],
@@ -57,7 +57,10 @@ def save_file(file_name, file_c, credentials):   # save login file
     f_obj.close()
     return "File %s saved!" % file_name
 
-def authenticate_new(credentials):
+def authenticate_new(credentials, authtries):
+    authtries += 1
+    if authtries > 5:
+        return "Failed!"
     param = {
         "agent": {
             "name": "Minecraft",
@@ -72,19 +75,28 @@ def authenticate_new(credentials):
     #    if file_text["errorMessage"]:
     #        print "Failed with error: %s" % file_text["errorMessage"]
     print "Received: " + str(file_text)
+    try:
+        if file_text["errorMessage"] == "Invalid credentials. Invalid username or password.":
+            credentials = {}
+            credentials["username"] = raw_input("Enter username: ")
+            credentials["password"] = raw_input("Enter password: ")
+            credentials["userid"] = "placeholder"
+            save_cred(cred_file, credentials)
+            authenticate_new(credentials, authtries)
+    except:
+        pass
     print "Successfully logged in with clienttoken: " + str(file_text["clientToken"])
     return file_text
 
 def validate_cur_session(file_c, credentials):
     param = {
-        "accessToken": file_c["authenticationDatabase"][credentials["userid"]]["accessToken"]
+        "accessToken": file_c["authenticationDatabase"][credentials["userid"]]["accessToken"],
+        "clientToken": file_c["clientToken"]
     }
-    req = requests.post(base_url + "/validate", data=json.dumps(param))
-    if req.text == "":
-        return True
-    else:
-        return False
-
+    req = requests.post(base_url + "/refresh", data=json.dumps(param))
+    print req.text
+    file_c["authenticationDatabase"][credentials["userid"]]["accessToken"] = req.text["accessToken"]
+    return file_c
 def invalidate_cur_session(file_c, credentials):
     param = {
         "accessToken": file_c["authenticationDatabase"][credentials["userid"]]["accessToken"],
@@ -111,28 +123,45 @@ except:
     print "Success!"
 
 
+
+file_c = load_file(login_file)
+print file_c
+print credentials
+try:
+    file_c = validate_cur_session(file_c, credentials)
+    print "Session valid!"
+except:
+    print "Unable to refresh session, creating new session..."
+    file_c = authenticate_new(credentials, authtries)
+    print "Saving " + login_file
+    save_file(login_file, file_c, credentials)
+
+"""
 try:
     file_c = load_file(login_file)
-    if validate_cur_session(file_c, credentials) == False:
-        print "Session invalid, invalidating..."
-        invalidate_cur_session(file_c, credentials)
-        print "Creating new session..."
-        file_c = authenticate_new(credentials)
-        print "Created new session, saving..."
-        save_file(login_file, file_c, credentials)
-        print "Success!"
-    else:
-        print "Session valid!"
+    file_c = validate_cur_session(file_c, credentials)
+    print "Session valid!"
+
+except:
+    print "Session invalid, invalidating..."
+    invalidate_cur_session(file_c, credentials)
+    print "Creating new session..."
+    file_c = authenticate_new(credentials, authtries)
+    print "Created new session, saving..."
+    save_file(login_file, file_c, credentials)
+    print "Success!"
+"""
+"""
 except:
     print "Error on loading %s" % login_file
     print "Creating new session..."
-    file_c = authenticate_new(credentials)
+    file_c = authenticate_new(credentials, authtries)
     print "Created new session, saving..."
     credentials["userid"] = file_c["selectedProfile"]["id"]
     save_file(login_file, file_c, credentials)
     save_cred(cred_file, credentials)
     print "Success!"
-
+"""
 """
 credentials = {}
 credentials["username"] = "philip.groet@gmail.com"
