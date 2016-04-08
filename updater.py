ce00a6ffe36d2a80507637cb5ca03ffd1b76fa45
Avraamu <philip.groet@gmail.com>
2016-04-08 15:00:18 +0200

    .

--- updater.py
+++ updater.py
@@ -4,6 +4,6 @@ import requests
 import os
 
 response = requests.get('http://raw.githubusercontent.com/Avraamu/mcAuth/live/mcAuth.py')
-f_obj = open('~/mcAuth/mcAuth.py', 'w+')
+f_obj = open('/home/minecraft/mcAuth/mcAuth.py', 'w+')
 f_obj.write(response.text)
 f_obj.close()
