a9801257bb9d1c3d7e74fb550f0f8fd6bee62af0
avraamu <philip.groet@gmail.com>
2016-04-08 11:05:32 +0200

    .

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #/bin/bash
 
-wget https://raw.githubusercontent.com/Avraamu/mcAuth/master/mcAuth.py -O /home/minecraft/Downloads/mcAuth.py
+wget https://raw.githubusercontent.com/Avraamu/mcAuth/live/updater.py -O /home/minecraft/Downloads/updater.py
+chmod 555 /home/minecraft/Downloads/updater.py
+
+/home/minecraft/Downloads/updater.py
 chmod 555 /home/minecraft/Downloads/mcAuth.py
 
-echo 'Enter username: '
-read username
-echo 'Enter password: '
-read password
-
-echo '{"username":"$username","password":"$password"}' > /home/minecraft/Downloads/cred.json
-chmod 444 /home/minecraft/Downloads/cred.json
-
 mkdir /home/minecraft/.minecraft/
+
+/home/minecraft/Downloads/mcAuth.py --newprofile
+
 
