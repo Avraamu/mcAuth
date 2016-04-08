801bd016911cd8be6d2078b8810cdb87a55dc5a2
avraamu <philip.groet@gmail.com>
2016-04-08 10:12:46 +0200

    .

--- mcAuth.py
+++ mcAuth.py
@@ -93,7 +93,7 @@ class Login:
         response = requests.post(url + "/authenticate", data=json.dumps(param))
         if response.status_code != 200:
             # throw error
-            logging.error('Could not authenticate!')
+            logging.error('Could not authenticate! ' + response.text)
             self.authenticated = False
             self.validClientToken = False
         else:
@@ -209,6 +209,7 @@ class Login:
 
 
 def defaultrun():
+    logging.debug('Default run...')
     try:
         try:
             obj.loadauth()
@@ -264,7 +265,9 @@ for opt, arg in opts:
         obj.clientToken = arg
 
     if opt in ('--update'):
+        logging.debug('Updating...')
         update()
+        sys.exit()
     elif opt in ('--newprofile'):
         print 'Username: '
         obj.username = raw_input()
@@ -279,7 +282,7 @@ for opt, arg in opts:
         else:
             logging.debug('Incorrect username or password')
         sys.exit()
-    if opt in ('--getmojangid'):
+    elif opt in ('--getmojangid'):
         obj.loadauth()  #Need a valid accessToken
         obj.getmojangid()
         obj.saveauth()  #Save mojangid to file
