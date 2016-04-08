a3cb6c9a9aa71093f62047819dbeb5f294ab3c77
root <root@pgvps2.(none)>
2016-04-08 06:26:04 -0400

    http more reliable

--- updater.py
+++ updater.py
@@ -3,7 +3,7 @@
 import requests
 import os
 
-response = requests.get('https://raw.githubusercontent.com/Avraamu/mcAuth/live/mcAuth.py')
+response = requests.get('http://raw.githubusercontent.com/Avraamu/mcAuth/live/mcAuth.py')
 f_obj = open('mcAuth.py', 'w+')
 f_obj.write(response.text)
 f_obj.close()
