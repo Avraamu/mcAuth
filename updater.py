170d6d9dbd5f70c4ca0b0d2b69e0673e73a4a802
avraamu <philip.groet@gmail.com>
2016-04-08 10:57:18 +0200

    typo

--- updater.py
+++ updater.py
@@ -5,5 +5,5 @@ import os
 
 response = requests.get('https://raw.githubusercontent.com/Avraamu/mcAuth/live/mcAuth.py')
 f_obj = open('mcAuth.py', 'w+')
-f_obj.write(reponse.text)
+f_obj.write(response.text)
 f_obj.close()
